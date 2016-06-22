7144a89ff74fb35b693e385081e06c3463c15a97
Mario Schuetz <mayo2@gmx.de>
2016-06-22 22:33:09 +0200

    add randomEdges()

--- src/Edge.java
+++ src/Edge.java
@@ -1,24 +1,13 @@
-
 public class Edge {
 	
-	private int weight;
 	private int cost;
 
-	public Edge(int weight, int cost){
+	public Edge(int cost){
 		this.cost = cost;
-		this.weight = weight;
-	}
-	
-	public int getWeight(){
-		return this.weight;
 	}
 	
 	public int getCost(){
 		return this.cost;
-	}
-	
-	public void setWeight(int weight){
-		this.weight = weight;
 	}
 	
 	public void setCost(int cost){
--- src/WeightedGraph.java
+++ src/WeightedGraph.java
@@ -1,30 +1,61 @@
 
 public class WeightedGraph {
 
-	private Edge edge;
 	private Edge[][] matrix;
+	private static int MAX_VERTICES = 20;
+	private static int MAX_EDGES = 40;
+	private static int MAX_COST = 5;
 
 	public static void main(String[] args) {
-		new WeightedGraph();
+		new WeightedGraph(MAX_VERTICES, MAX_EDGES);
 	}
 
-	public WeightedGraph() {
-		int vertices = (int) (Math.random() * 10 + 1);
-		System.out.println("random# of vertices " +vertices);
+	public WeightedGraph(int vertices, int edge) {
 		matrix = new Edge[vertices][vertices];
+		randomEdges();
+		printMatrix();
+
 	}
 
-	private void addEdge() {
-
+	private void addEdge(int x, int y, Edge edgeToAdd) {
+		matrix[x][y] = edgeToAdd;
+	}
+	
+	private Edge getEdge(int x, int y){
+		return matrix[x][y];
 	}
 
-	private void setEdgeWeightCost(int edgeName, int weight, int cost) {
-		edge.setCost(cost);
-		edge.setWeight(weight);
+	private void randomEdges() {
+		int edgeCnt = 0;
+		do {
+			for (int x = 1; x < MAX_VERTICES; x++) {
+				for (int y = 1; y < MAX_VERTICES; y++) {
+					int decision = (int) (Math.random() * 5);
+					if (decision == 1) {
+						addEdge(x, y, new Edge((int) (Math.random() * MAX_COST + 1)));
+						edgeCnt++;
+					} else {
+						addEdge(x, y, null);
+					}
+				}
+			}
+		} while (edgeCnt < MAX_EDGES);
 	}
 
-	private void addVertex() {
+	public void printMatrix() {
 
+		int edge = 0;
+		for (int x = 1; x < MAX_VERTICES; x++) {
+			for (int y = 1; y < MAX_VERTICES; y++) {
+				if (getEdge(x, y) != null)
+					edge = getEdge(x,y).getCost();
+				else
+					edge = 0;
+
+				System.out.print(" " + edge);
+			}
+			System.out.println("");
+		}
 	}
 
 	private boolean containsEdge() {
